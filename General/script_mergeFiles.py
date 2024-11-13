250d7ac99d5ed5e19d5cfbb5fda5b406a58a486c
Cesar Bernardes <cesar.augusto.bernardes@cern.ch>
2024-11-13 10:36:52 -0300

    Update script_mergeFiles.py

--- General/script_mergeFiles.py
+++ General/script_mergeFiles.py
@@ -19,6 +19,21 @@ for line1 in f1.readlines():
         if columns1[0]==columns2[0] and columns1[1]==columns2[1] and columns1[2]==columns2[2]:
             line1 = line1.replace(columns1[3],columns2[3])
     array_text.append(line1)
+
+
+###to add missing APVs
+f2.seek(0)
+for line3 in f2.readlines():
+    foundAPV = False
+    columns3 = line3.split(column_separator)  # Get the data groups
+    f1.seek(0)
+    for line4 in f1.readlines():
+        columns4 = line4.split(column_separator)  # Get the data groups
+        if columns3[0]==columns4[0] and columns3[1]==columns4[1] and columns3[2]==columns4[2]:
+            foundAPV = True
+    if foundAPV==False:
+        array_text.append(line3)
+
 f3.writelines(array_text)
 f1.close()
 f2.close()
